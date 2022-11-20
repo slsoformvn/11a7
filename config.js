723027dd3584131c9f20527293ec8eda0bf1a194
slsoformvn <85289441+slsoformvn@users.noreply.github.com>
2022-11-20 12:01:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'http://thpthoangmai.edu.vn/';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Gửi Cô Minh và lớp 11A7';
+const contentCard = ' Nhân ngày 20/11,em xin chúc các thầy các cô lời chúc tốt đẹp nhất. Chúc các thầy cô hoàn thành nhiệm vụ cao cả của mình trong sự nghiệp trăm năm trồng người, trong công cuộc đổi mới của đất nước. Em chân thành cám ơn các thầy cô.';
+
+const giftImage = 'https://lh3.googleusercontent.com/pw/AL9nZEXuwUIJ5RW_ogfNk2ol-nYO-TS56pFbJGm73V6HwfxpbTvV5nopyBP0Vo7lmouB_qpSe69cHvzM1_Dn-WDKlegVk1Bib71H25F6a_6Jijxj1Dn7YbyKPioG0zAI2O-G7XZgVNivOo-Vzh4W7rTQej0cJ0XeJ6VCiyq_A2WK1vyc_ykQ3Gr5xFGP9B7gVME3ZojNFggwUmmGMpM8_V-oihN33dzj8muXSkIaHBCCyl7xnlEW0SaWksNakZ0ktJVxkm7Q28IRVpg76BWe0k3xAobrHxqvXAruHRQY7X3WcQia_ygl4FXHnF_rTYL5sw1jKVFd80yySCsII0ql9uMLiQb19eWEATbOOWe4frG5p73A8TDIMCGTNqPfCg-FN-TsZjVuSz2q3rwtXlMaaVYoOwIyv4rXqdfhtYTKcrkRepxxArEof1jgpBkjoi_lJ6jRmxIUPXVyUIqMUJzOtDDSAyr6SVDCKpGp9TgwLGiKtc4N2ssuTeB12yeHnMvwOtCrG126OesmTdED39xpGI0j22DM5O_XZclgd8C1s-zLOCQOX6OfzXD3ip4uWebza2TlCdryDDUuN8vEs_I9TDhFCJyt-OPAPoittzGn8Yfj1Pj0jTry8kPW-z3ETpe8rVDhDWs4P0JFI-YcCApdgDPy02gov5th3qUHDF6YWGL_tPZnqq44xG6uHykCQVWNptfc81Va3i2vTtiylSFnOX4Jxjx6up8Bf1vrf0OfxItMGbROuIrPoGQnozfehbwCrCrYQZODCaxYUzqBkxkhZJE2svo9sxTE-VtkzOJyRCxtH2cshxZWE9TBBeKSPYNGDUgqdC8FkQJnvBBuwYIPAmw-wDid9pB7e5Bt2MBg3-i_GEvEwahJBz4AUzE-Hgi5hXG9kkCxWdu8Dsp3U4Vye4l2hujTRYpUpf2p7yUHl4Op3xq3w23S-v8BSZLhNB4S4AgxvJEbikltPLDN5p87WvcoSeY6yHg3SQ=w614-h920-no?authuser=0';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
